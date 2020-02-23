73d110318a52b2638ec3965bdf1e5c71debff207
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-02-23 13:55:04 +0600

    Add files via upload

---
+++ ajax-subcribs/js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+
+	$('.mailchim-form').ajaxChimp({
+
+		url:'https://gmail.us4.list-manage.com/subscribe/post?u=f7f60855335a1543ad1952912&amp;id=5d52eb000d'
+	});
+});
