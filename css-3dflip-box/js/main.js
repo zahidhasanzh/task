3a28b2cba401ef24ac130cc249acd465ad61ab3b
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-02-23 23:18:23 +0600

    Add files via upload

---
+++ css-3dflip-box/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$('.hover').hover(function(){
+	$(this).addClass('flip');
+},function(){
+	$(this).removeClass('flip');
+});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
