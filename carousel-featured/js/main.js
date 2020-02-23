b5a9fc2a40d73525e6dd3d8741515de2c760d715
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-02-23 23:03:11 +0600

    Add files via upload

---
+++ carousel-featured/js/main.js
@@ -0,0 +1,23 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".testimonial-carousel").owlCarousel({
+
+		items:1,
+		dots:false,
+		nav:true,
+		navText:["<i class='fa fa-angle-left'>","<i class='fa fa-angle-right'>"],
+		loop:true,
+		autoPlay:false,
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
