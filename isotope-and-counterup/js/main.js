edd87351e86b9a27a66e804f94f4ab1a53bb7352
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-02-24 16:19:02 +0600

    Add files via upload

---
+++ isotope-and-counterup/js/main.js
@@ -0,0 +1,35 @@
+
+(function ($) {
+	"strict";
+
+
+$(document).ready(function($){
+	$('.project-titels li').on('click',function(){
+		$(".project-titels li").removeClass("active")
+		$(this).addClass("active");
+			var selector =$(this).attr('data-filter');
+	 	$(".new-projects-list").isotope({
+	 		filter:selector
+		});
+	});
+
+
+
+
+	});
+
+var $grid = $('.new-projects-list').isotope({
+  // options
+});
+
+
+  jQuery(document).ready(function($) {
+            $('.counter-number span').counterUp({
+                delay: 10,
+                time: 1000
+            });
+        });
+
+
+
+}(jQuery));
