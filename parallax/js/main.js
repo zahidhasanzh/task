5d26de78ba4fc9b780edcdb61975c4570fb68d35
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-02-26 23:16:26 +0600

    Add files via upload

---
+++ parallax/js/main.js
@@ -0,0 +1,18 @@
+// (function ($) {
+// 	"strict";
+
+
+// $(document).ready(function($){
+	
+// 	var s =skrollr.init({
+// 		render:function(data)
+
+// 	});
+//  });
+
+// jQuery(window).load(function(){
+
+// });
+
+
+// }(jQuery));
