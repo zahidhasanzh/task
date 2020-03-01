bf48756a09f88bed12aecc485f0d2dc0deca14d9
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-03-01 23:33:50 +0600

    Add files via upload

---
+++ progress-bar/js/main.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $('#bar1').barfiller({
+    	barColor:'#fff'
+    });
+     $('#bar2').barfiller({
+    	barColor:'#fff'
+    });
+      $('#bar3').barfiller({
+    	barColor:'#fff'
+    });
+       $('#bar4').barfiller({
+    	barColor:'#fff'
+    });
+});
