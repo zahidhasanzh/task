e18316943ac76f688864ee8bd03bd31ca2aa414f
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-03-01 23:32:33 +0600

    Add files via upload

---
+++ product-carousel-1/js/main.js
@@ -0,0 +1,22 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$('.featured-trailors-carousel').owlCarousel({
+		items:1,
+		autoPlay:false,
+		loop:true,
+		nav:false,
+		dots:true,
+
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
