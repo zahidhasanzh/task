287a6cbca7457be0ef4b4a1152647d507519f858
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-03-09 16:25:29 +0600

    Add files via upload

---
+++ testomonial-tab/js/main.js
@@ -0,0 +1,24 @@
+
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	const ps = new PerfectScrollbar('.testomonial-author-name');
+
+
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
+
+
+
+
+
+
+
+
