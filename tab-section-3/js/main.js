860a346908a2581b1ac42c3a0358a50b4bf8b3ab
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-03-09 16:14:37 +0600

    Add files via upload

---
+++ tab-section-3/js/main.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".featured-projects").owlCarousel({
+
+		items:1,
+		nav:false,
+		dots:true,
+		loop:true,
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
