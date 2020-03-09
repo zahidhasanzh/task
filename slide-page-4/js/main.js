ea2f324365d30cb5fa0a3cab0434ecd5032391e5
Zahid <61347323+zahidhasanzh@users.noreply.github.com>
2020-03-09 16:09:40 +0600

    Add files via upload

---
+++ slide-page-4/js/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	"strict";
+
+$(document).ready(function($){
+
+	$(".homepage-slide").owlCarousel({
+
+		items:1,
+		dots:true,
+		nav:true,
+		navText:["<i class='fa fa-angle-left'</i>", "<i class='fa fa-angle-right'</i>"],
+		loop:true,
+		autoplay:false,
+		dotsData:true,
+		
+	});
+
+	
+});
+
+jQuery(window).load(function(){
+
+});
+
+}(jQuery));
